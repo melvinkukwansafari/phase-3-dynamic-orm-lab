2c816d7ab00bf70ccee35521fbe32af4fd208a88
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-03-01 12:37:28 +0300

    complete assignment

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,13 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
-end
+    self.column_names.each do |col_name|
+      attr_accessor col_name.to_sym
+    end
+
+
+     def self.find_by(attribute)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0]} = ?"
+    DB[:conn].execute(sql, attribute.values[0])
+    end
+  end
